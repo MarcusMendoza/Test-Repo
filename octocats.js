205a8ff396352b801b5e130d5f2a173405b656a2
MarcusMendoza <7mmendoza@stpatrick.org>
2016-10-06 14:10:33 -0500

    Octocat day 1 of coding

---
+++ octocats.js
@@ -0,0 +1,23 @@
+//Write your constructor function below.
+function octocat(arms, name){
+this.name=boopy;
+this.arms=arm;
+this.slap= function slap(){
+  for (arm=8,arm>0,arm--)
+  console.log (name+"just sn")
+}
+
+}
+//You should have 2 instance variables and 1 instance function included.
+
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
