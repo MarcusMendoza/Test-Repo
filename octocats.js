65fa70a41efa626d355b577fd03091cdac622746
MarcusMendoza <7mmendoza@stpatrick.org>
2016-10-07 12:53:44 -0500

    day 2 of octocat creation

--- octocats.js
+++ octocats.js
@@ -1,14 +1,15 @@
 //Write your constructor function below.
 function octocat(arms, name){
 this.name=boopy;
-this.arms=arm;
-this.slap= function slap(){
-  for (arm=8,arm>0,arm--)
-  console.log (name+"just sn")
+this.arms=a;
+this.slap= function(){
+  for (a=0;this.arms<40;a++)
+  console.log (this.name+"just slapped you on the back and will try to do it again")
 }
 
 }
 //You should have 2 instance variables and 1 instance function included.
+
 
 
 
